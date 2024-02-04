fbc6a8eeb5a22b8ae455438a0131dc76ebe72506
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2024-02-04 13:29:24 +0100

    feat: add printing version increment help

--- src/main/groovy/cli/CliHandler.groovy
+++ src/main/groovy/cli/CliHandler.groovy
@@ -5,18 +5,27 @@ import version.Version
 
 class CliHandler {
 
+    def final static FORMAT = ' %-15s%s\n'
+
     def cli
     def options
     def args
 
     CliHandler(args) {
         this.args = args
-        this.cli = new CliBuilder(usage: 'versioner-q -[options]')
+        this.cli = new CliBuilder(usage: 'versioner-q -[options] <versionType>')
 
         cli.with {
             h longOpt: 'help', 'Show usage information'
             v longOpt: 'version', 'Show software version'
             g longOpt: 'gradle', 'Update Gradle project version'
+        }
+
+        cli.metaClass.versionTypes() {
+            println '\nPossible version types: '
+            printf FORMAT, 'major', 'Increments major version number'
+            printf FORMAT, 'minor', 'Increments minor version number'
+            printf FORMAT, 'bugfix', 'Increments bugfix version number'
         }
     }
 
@@ -25,6 +34,7 @@ class CliHandler {
 
         if (options.h) {
             cli.usage()
+            cli.versionTypes()
         }
 
         if (options.v) {
