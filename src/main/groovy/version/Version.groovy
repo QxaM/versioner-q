50ca9f3bb6e58651b984fc5275782e0fff78733f
Piotr Gliszczynski <gliszczynskip5@gmail.com>
2024-02-04 12:37:39 +0100

    feat: add version handling

---
+++ src/main/groovy/version/Version.groovy
@@ -0,0 +1,19 @@
+package version
+
+class Version {
+
+    def static final VERSION = '0.0.0.1'
+
+    static printVersion() {
+        printVersionQ()
+        printGroovyVersion()
+    }
+
+    static printVersionQ() {
+        println 'Version-Q: ' + VERSION
+    }
+
+    static printGroovyVersion() {
+        println 'Groovy: ' + GroovySystem.getVersion()
+    }
+}
